5f6e21d40852f7df0c9375070a360d6317ed76a6
陈航 <chenhang@chen.local>
2021-02-02 00:38:37 +0800

    feat: 增加swagger文档方案

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import * as rateLimit from 'express-rate-limit';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// api文档插件
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 // 替换 console 为更统一友好的
 const { log, warn, info } = console;
 const color = c => c
@@ -20,6 +23,19 @@ async function bootstrap() {
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
   }));
+  // https://blog.csdn.net/weixin_43902189/article/details/102748102
+  // DocumentBuilder是一个辅助类，有助于结构的基本文件SwaggerModule。它包含几种方法，可用于设置诸如标题，描述，版本等属性。
+  const options = new DocumentBuilder()
+    .setTitle('DFYJ论坛接口文档')
+    .setDescription('使用Nest书写的Restful接口') // 文档介绍
+    .setVersion('1.0.0') // 文档版本
+    .addTag('用户,安全') // 每个tag标签都可以对应着几个@ApiUseTags('用户,安全') 然后被ApiUseTags注释，字符串一致的都会变成同一个标签下的
+    // .setBasePath('http://localhost:5000')
+    .build();
+  // 为了创建完整的文档（具有定义的HTTP路由），我们使用类的createDocument()方法SwaggerModule。此方法带有两个参数，分别是应用程序实例和基本Swagger选项。
+  const document = SwaggerModule.createDocument(app, options)
+  // 最后一步是setup()。它依次接受（1）装入Swagger的路径，（2）应用程序实例, （3）描述Nest应用程序的文档。
+  SwaggerModule.setup('/api', app, document)
   // app.useStaticAssets(join(__dirname, '..', 'public'), {
   //   prefix: '/static/',   //设置虚拟路径
   // });
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,12 +7,12 @@ import { SbUsers } from './users.entity'
 @Injectable()
 export class UsersService implements OnModuleInit {
   constructor(@InjectRepository(SbUsers)
-    private readonly usersRepository: Repository<SbUsers>) { }
+    private readonly SbUsersRepository: Repository<SbUsers>) { }
 
   async onModuleInit() {
     if (await this.findOneByUsername('admin')) return
     // 初始化系统管理员
-    const admin = this.usersRepository.create({
+    const admin = this.SbUsersRepository.create({
       username: 'admin',
       password: '123',
       email: 'star_ch1028@163.com',
@@ -22,7 +22,54 @@ export class UsersService implements OnModuleInit {
       nickName: 'admin',
       realName: 'admin'
     })
-    await this.usersRepository.save(admin)
+    await this.SbUsersRepository.save(admin)
+  }
+
+  /**
+   * 注册用户
+   * @param user 用户信息 
+   */
+  async register(user: SbUsers): Promise<void> {
+    const existing = await this.findOneByUsername(user.username)
+    if (existing) {
+      throw new HttpException('账号已存在', 409)
+    }
+    // 加密处理密码
+    user.password = ''
+    await this.SbUsersRepository.save(this.SbUsersRepository.create(user)) 
+  }
+
+  /**
+   * 判断用户是否可以登陆
+   * @param username 
+   * @param password 
+   */
+  async login(username: string, password: string): Promise<void> {
+
+  }
+
+  /**
+   * 更新用户信息
+   * @param user 
+   */
+  async update(uid: number, user: SbUsers): Promise<void> {
+    const existing = await this.SbUsersRepository.findOne({ uid })
+    if (!existing) {
+      throw new HttpException(`更新失败, ID为${uid}的用户不存在`, 404)
+    }
+    await this.SbUsersRepository.save(user)
+  }
+
+  async delete(uid: number): Promise<void> {
+    const existing = await this.SbUsersRepository.findOne({ uid })
+    if (!existing) {
+      throw new HttpException(`删除失败, ID为${uid}的用户不存在`, 404)
+    }
+    await this.SbUsersRepository.remove(existing)
+  }
+
+  async findOneByUid(uid: number): Promise<SbUsers> {
+    return await this.SbUsersRepository.findOne({ uid })
   }
 
   /**
@@ -30,6 +77,18 @@ export class UsersService implements OnModuleInit {
    * @param username 
    */
   async findOneByUsername(username: string): Promise<SbUsers> {
-    return await this.usersRepository.findOne({ username })
+    return await this.SbUsersRepository.findOne({ username })
+  }
+
+  async findOneByEmail(email: string): Promise<SbUsers> {
+    return await this.SbUsersRepository.findOne({ email })
+  }
+
+  /**
+   * 分页获取数据
+   */
+  async findAllUsers(pageNumber: number, pageSize: number) {
+    const db = this.SbUsersRepository.createQueryBuilder('users')
+    return await db.skip(pageSize * (pageNumber - 1)).take(pageSize).orderBy({'users.id': 'DESC'}).getManyAndCount()
   }
 }
